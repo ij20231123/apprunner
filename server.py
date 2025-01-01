37b7107d969e1f8b0ca727a76885a0ce8a828e70
Sathiesh Veera <sathyvsk@gmail.com>
2025-01-01 08:07:04 -0700

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 def hello_world(request):
     name = os.environ.get('NAME')
@@ -12,9 +15,12 @@ def hello_world(request):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
+    logger.info(f"running main application and listening on port {os.environ.get("PORT")}")
     with Configurator() as config:
+        logger.info("configuring apis")
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
+    logger.info("starting server")
     server.serve_forever()
